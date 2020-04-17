7fdc291e8089d4f905431c03e059e91f508d2981
Swapnil <swapnilme@Cybage.com>
2020-04-17 14:13:17 +0530

    filed added contacts about

--- src/components/cart/courseCart.js
+++ src/components/cart/courseCart.js
@@ -45,7 +45,7 @@ class courseCart extends Component {
                                   data-item-price ={node.price}
                                   data-item-catagory={node.catagory}
                                   data-item-url="https://swapnilme.netlify.app/"
-                                  data-item-discription={node.discription.discription}
+                                //data-item-discription={node.discription.discription}
                                  >Join Now</button>
                                  
                              </Card>
---
+++ src/pages/contact.js
@@ -0,0 +1,52 @@
+import React from "react"
+//import { Link } from "gatsby"
+import { graphql} from 'gatsby'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/resusableComponent/HeroSection";
+import Infoblock from "../components/resusableComponent/Infoblock"
+import CourseCart from "../components/cart/courseCart";
+
+const IndexPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection title="hero" subtitle="ironman" class1="hero-background" img={data.img.childImageSharp.fluid}/>
+    <Infoblock heading="About Us"/>
+    <CourseCart courses={data.mycourses}/>
+  </Layout>
+)
+
+export const query = graphql`
+{
+  img: file(relativePath: { eq: "heromain.png" }) {
+    childImageSharp {
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+
+
+  mycourses: allContentfulCourses {
+    edges {
+      node {
+        
+        id
+        title
+        price
+        catagory
+        discription{
+          discription
+        }
+        images{
+          fixed(width:200, height:120){
+            ...GatsbyContentfulFixed_tracedSVG
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export default IndexPage
